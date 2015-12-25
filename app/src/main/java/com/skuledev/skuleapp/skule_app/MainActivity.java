6680cb5ddfd4287d7f61aaff2f95d3a94fb7da13
Abhinav Rajaseshan <abhinavseshan@hotmail.com>
2015-12-25 14:20:50 -0500

    Added Starter code and activity linking

--- app/src/main/java/com/skuledev/skuleapp/skule_app/MainActivity.java
+++ app/src/main/java/com/skuledev/skuleapp/skule_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.skuledev.skuleapp.skule_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
             }
         });
+
+        Button eventsButton = (Button) findViewById(R.id.Events);
+        eventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MainActivity.this, EventsActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        Button clubslist = (Button) findViewById(R.id.clubslist);
+        clubslist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MainActivity.this, ClubsListActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        Button clubsButton = (Button) findViewById(R.id.club);
+        clubsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MainActivity.this, ClubActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
     }
 
     @Override
